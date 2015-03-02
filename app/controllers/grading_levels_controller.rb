66677837521a9e3fd3cd6689766a0704358d973b
Your Name <you@example.com>
2015-03-02 23:55:50 +0530

    develop commit

--- app/controllers/grading_levels_controller.rb
+++ app/controllers/grading_levels_controller.rb
@@ -2,17 +2,24 @@
 class GradingLevelsController < ApplicationController
   before_filter :find_batch, only: [:new, :create]
   before_filter :find_grade, only: [:edit, :update, :destroy]
+  
+  # This method is used for Display all batches including courses
   def index
     @batches ||= Batch.includes(:course).all
     authorize! :read, @batches.first
   end
 
+  # This method is used for get new grading level by
+  # maintain association of batch and grading level
   def new
     @grading_level = GradingLevel.new
     @grading_level1 = @batch.grading_levels.build
     authorize! :create, @grading_level
   end
 
+  # This method is used for creating gradin level,
+  #  pass required params
+  # from private method and save grading level
   def create
     @grading_levels ||= @batch.grading_levels
     @grading_level1 = @batch.grading_levels.new(params_grade)
@@ -20,16 +27,22 @@ class GradingLevelsController < ApplicationController
     flash[:notice] = t('grade_create')
   end
 
+  # this method is used to edit grading level
   def edit
     authorize! :update, @grading_level1
   end
 
+  # edit grading level,first find grading_level which to be edit
+  # and transfer controll to update method
   def update
     @grading_levels ||= @batch.grading_levels
     @grading_level1.update(params_grade)
     flash[:notice] = t('grade_update')
   end
 
+  # This method is used for destroying grading level,
+  # first find grading level to be deleted,
+  # call destroy method on instance of grading level and perform authorization
   def destroy
     authorize! :delete, @grading_level1
     @grading_level1.destroy
@@ -37,6 +50,8 @@ class GradingLevelsController < ApplicationController
     redirect_to grading_levels_path
   end
 
+  # this method is used to select batch and hold all
+  # grading levels of selected batch
   def select
     @batch = Batch.shod(params[:batch][:id])
     @grading_levels ||= @batch.grading_levels
